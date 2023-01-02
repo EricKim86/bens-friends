763c181ebf0374ba0ea12a41c5fa6175da66adab
dpinkus87 <dylanpinkus@gmail.com>
2023-01-02 09:18:12 -0600

    Updated some seed data

--- seeds/user.Data.js
+++ seeds/user.Data.js
@@ -9,7 +9,15 @@ const userData = [
         password: 'test123456789',
         city: 'North Wales',
         bio: 'My name is Eric and I assisted in designing this application.  Our collective goal was to create a place where individuals can come together and celebrate their appreciation for Ben.'
-    },
+    }, {
+        user_name: 'Dylan',
+        email: 'Dylan@email.com',
+        password: 'test123456789',
+        city: 'Milwaukee',
+        bio: 'Our goal for this project was to create a social/e-commerce platform where users could purchase items they find interesting and share what they bought with their connections. We happen to all own items featuring Ben.'
+    }
+
+
 ]
 const seedUser = () => User.bulkCreate(userData);
 
